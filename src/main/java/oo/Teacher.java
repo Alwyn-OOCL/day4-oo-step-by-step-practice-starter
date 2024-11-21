2040f3d3f0bbe7cd28d201d2438cc7b5e294ae13
HEAL <HEAL@OOCL.COM>
2024-11-21 14:17:16 +0800

    [Step by step] step4: pass should_return_true_when_belongsTo_given_teacher_teaches_in_the_class and should_return_message_with_name_age_and_class_when_introduce_given_teacher_teaches_multiple_classes test in StudentTest

--- src/main/java/oo/Teacher.java
+++ src/main/java/oo/Teacher.java
@@ -1,6 +1,12 @@
 package oo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Teacher extends Person {
+
+    private List<Klass> klass;
 
     public Teacher(int id, String name, int age) {
         super(id, name, age);
@@ -8,6 +14,18 @@ public class Teacher extends Person {
 
     @Override
     public String introduce() {
-        return super.introduce() + " I am a teacher.";
+        String klasses = klass.stream().map(Klass::getId).map(String::valueOf).collect(Collectors.joining(", "));
+        return super.introduce() + " I am a teacher. I teach Class " + klasses + ".";
+    }
+
+    public boolean belongsTo(Klass klass) {
+        return this.klass != null && this.klass.contains(klass);
+    }
+
+    public void assignTo(Klass klass) {
+        if (this.klass == null) {
+            this.klass = new ArrayList<>();
+        }
+        this.klass.add(klass);
     }
 }
