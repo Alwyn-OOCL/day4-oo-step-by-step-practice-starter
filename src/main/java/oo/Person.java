6cf17f2a3be65bd8342b8b11064de8f2fb403385
HEAL <HEAL@OOCL.COM>
2024-11-21 13:43:32 +0800

    [Step by step] finish step 1

--- src/main/java/oo/Person.java
+++ src/main/java/oo/Person.java
@@ -1,4 +1,42 @@
 package oo;
 
+import java.util.Objects;
+
 public class Person {
+    private Integer id;
+
+    private String name;
+
+    private int age;
+
+    public Person(Integer id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    public String introduce() {
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Person person = (Person) o;
+        return Objects.equals(id, person.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
 }
