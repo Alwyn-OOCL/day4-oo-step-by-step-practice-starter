6433c8359bf778069f87413e2209395704101d5a
HEAL <HEAL@OOCL.COM>
2024-11-21 14:03:19 +0800

    [Step by step] step3: pass test in KlassTest

--- src/main/java/oo/Klass.java
+++ src/main/java/oo/Klass.java
@@ -1,4 +1,32 @@
 package oo;
 
+import java.util.Objects;
+
 public class Klass {
+
+    private Integer id;
+
+    public Klass(Integer id) {
+        this.id = id;
+    }
+
+    public Klass() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Klass klass = (Klass) o;
+        return Objects.equals(id, klass.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
