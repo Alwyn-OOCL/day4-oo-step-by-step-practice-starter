b5da94f2dfadea49fed32129a3d4c6444248ae44
HEAL <HEAL@OOCL.COM>
2024-11-21 14:42:05 +0800

    [Step by step] refactor: step6: pass all test in KlassTest

--- src/main/java/oo/Klass.java
+++ src/main/java/oo/Klass.java
@@ -1,5 +1,7 @@
 package oo;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Klass {
@@ -7,6 +9,8 @@ public class Klass {
     private Integer id;
 
     private Student leader;
+
+    private List<Person> participants;
 
     public Klass(Integer id) {
         this.id = id;
@@ -46,10 +50,24 @@ public class Klass {
             return;
         }
         this.leader = student;
+        participants.forEach(person -> {
+            if (person instanceof Teacher) {
+                System.out.print("I am " + person.getName() + ", teacher of Class " + id + ". I know " + student.getName() + " become Leader.\n");
+            }
+            if (person instanceof Student) {
+                System.out.print("I am " + person.getName() + ", student of Class " + id + ". I know " + student.getName() + " become Leader.\n");
+            }
+        });
     }
 
     public boolean isLeader(Student student) {
         return leader != null && leader.equals(student);
     }
 
+    public void attach(Person person) {
+        if (participants == null) {
+            participants = new ArrayList<>();
+        }
+        participants.add(person);
+    }
 }
